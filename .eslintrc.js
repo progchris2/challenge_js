0429d5513ac59b57f54f3851526a19062ffc0c9a
Gilles Chris <progchris>
2020-08-25 16:23:38 +0200

    modification .gitignore ajout eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+	browser: true,
+	es6: true
+  },
+  extends: [
+	'plugin:react/recommended',
+	'plugin:prettier/recommended'
+  ],
+  globals: {
+	Atomics: 'readonly',
+	SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+	ecmaFeatures: {
+	  jsx: true
+	},
+	ecmaVersion: 2019,
+	sourceType: 'module'
+  },
+  plugins: [
+	'react',
+	'@typescript-eslint',
+  ],
+  rules: {
+	'prettier/prettier': [
+	  'error',
+	  {
+		'singleQuote': false,
+		'jsxSingleQuote': false,
+		'tabWidth': 4,
+		'trailingComma': 'all',
+		'Semicolons': true,
+		'JSX Brackets': false
+	  }
+	]
+  }
+}
